4ce2b60a417103165b7fa6936a70e41a6787b762
Tanner Snow <53470063+Neutronlul@users.noreply.github.com>
2025-12-14 17:18:00 -0800

    More clangd

--- DoublyLinkedList(Names)/main.cpp
+++ DoublyLinkedList(Names)/main.cpp
@@ -1,33 +1,34 @@
-#include <fstream>
-#include <string>
-#include <iostream>
 #include <algorithm>
 #include <cctype>
-
+#include <fstream>
+#include <iostream>
+#include <string>
 
 struct Node {
-    std::string name;
-    Node* next = nullptr;
-    Node* prev = nullptr;
+  std::string name;
+  Node *next = nullptr;
+  Node *prev = nullptr;
 };
 
 struct NodeList {
-    Node* head = nullptr;
-    Node* tail = nullptr;
+  Node *head = nullptr;
+  Node *tail = nullptr;
 };
 
-// traverses through the list and returns the node that the new node should be inserted before
-Node* findInsertPosition (NodeList &list, const std::string &data) {
-    // start at the node after the head because insertNode() already checked the head
-    Node* curr = list.head->next;
+// traverses through the list and returns the node that the new node should be
+// inserted before
+Node *findInsertPosition(NodeList &list, const std::string &data) {
+  // start at the node after the head because insertNode() already checked the
+  // head
+  Node *curr = list.head->next;
 
-    // traverse through the list until the name to insert is less than curr->name
-    while (data > curr->name) {
-        curr = curr->next;
-    }
+  // traverse through the list until the name to insert is less than curr->name
+  while (data > curr->name) {
+    curr = curr->next;
+  }
 
-    // return curr, which should come after the new node in the list
-    return curr;
+  // return curr, which should come after the new node in the list
+  return curr;
 }
 
 /*
@@ -37,38 +38,39 @@ if(!head){ //no other nodes
 }else{ //somewhere in the middle
 }
 */
-void insertNode (NodeList &list, const std::string &data) {
-    if (!list.head) {
-        Node* newNode = new Node{data, nullptr, nullptr};
-        list.head = newNode;
-        list.tail = newNode;
-    } else if (data < list.head->name) {
-        Node* newNode = new Node{data, list.head, nullptr};
-        list.head->prev = newNode;
-        list.head = newNode;
-    } else if (data > list.tail->name) {
-        Node* newNode = new Node{data, nullptr, list.tail};
-        list.tail->next = newNode;
-        list.tail = newNode;
-    } else {
-        Node* insertBefore = findInsertPosition(list, data);
-        Node* newNode = new Node{data, insertBefore, insertBefore->prev};
-        insertBefore->prev->next = newNode;
-        insertBefore->prev = newNode;
-    }
+void insertNode(NodeList &list, const std::string &data) {
+  if (!list.head) {
+    Node *newNode = new Node{data, nullptr, nullptr};
+    list.head = newNode;
+    list.tail = newNode;
+  } else if (data < list.head->name) {
+    Node *newNode = new Node{data, list.head, nullptr};
+    list.head->prev = newNode;
+    list.head = newNode;
+  } else if (data > list.tail->name) {
+    Node *newNode = new Node{data, nullptr, list.tail};
+    list.tail->next = newNode;
+    list.tail = newNode;
+  } else {
+    Node *insertBefore = findInsertPosition(list, data);
+    Node *newNode = new Node{data, insertBefore, insertBefore->prev};
+    insertBefore->prev->next = newNode;
+    insertBefore->prev = newNode;
+  }
 }
 
-Node* findMatchingNode (NodeList &list, const std::string &data) {
-    // start at the head
-    Node* curr = list.head;
+Node *findMatchingNode(NodeList &list, const std::string &data) {
+  // start at the head
+  Node *curr = list.head;
 
-    // traverse through the list until a matching name is found or the end is reached
-    while (curr && curr->name != data) {
-        curr = curr->next;
-    }
+  // traverse through the list until a matching name is found or the end is
+  // reached
+  while (curr && curr->name != data) {
+    curr = curr->next;
+  }
 
-    // returns nullptr if no such node exists
-    return curr;
+  // returns nullptr if no such node exists
+  return curr;
 }
 
 // student may be at the head, the tail or in the middle
@@ -80,114 +82,116 @@ to be more specific, there are 5 cases:
 4. node is in the middle
 5. node does not exist/list is empty
 */
-void deleteNode (NodeList &list, const std::string &data) {
-    Node* deletionTarget = findMatchingNode(list, data);
+void deleteNode(NodeList &list, const std::string &data) {
+  Node *deletionTarget = findMatchingNode(list, data);
 
-    // case 5: handle no matching node; also handles empty list
-    if (deletionTarget) {
-        // case 1/2: node is the head
-        if (deletionTarget == list.head) {
-            // advance the head
-            list.head = list.head->next;
+  // case 5: handle no matching node; also handles empty list
+  if (deletionTarget) {
+    // case 1/2: node is the head
+    if (deletionTarget == list.head) {
+      // advance the head
+      list.head = list.head->next;
 
-            // case 2: if the list is not empty at this point, set the new head's prev pointer to nullptr
-            if (list.head) {
-                list.head->prev = nullptr;
-            }
-        } else {
-            // case 3/4: node is in the middle or at the tail
-            // set the next pointer of the node before the
-            // deletion target to the node after it
-            deletionTarget->prev->next = deletionTarget->next;
-        }
-        // case 1/3: node is the tail
-        if (deletionTarget == list.tail) {
-            // advance (retreat?) the tail
-            list.tail = list.tail->prev;
-
-            // case 3: if the list is not empty at this point, set the new tail's next pointer to nullptr
-            if (list.tail) {
-                list.tail->next = nullptr;
-            }
-        } else {
-            // case 2/4: node is in the middle or at the head
-            // set the prev pointer of the node after the
-            // deletion target to the node before it
-            deletionTarget->next->prev = deletionTarget->prev;
-        }
-
-        delete deletionTarget;
+      // case 2: if the list is not empty at this point, set the new head's prev
+      // pointer to nullptr
+      if (list.head) {
+        list.head->prev = nullptr;
+      }
+    } else {
+      // case 3/4: node is in the middle or at the tail
+      // set the next pointer of the node before the
+      // deletion target to the node after it
+      deletionTarget->prev->next = deletionTarget->next;
     }
+    // case 1/3: node is the tail
+    if (deletionTarget == list.tail) {
+      // advance (retreat?) the tail
+      list.tail = list.tail->prev;
+
+      // case 3: if the list is not empty at this point, set the new tail's next
+      // pointer to nullptr
+      if (list.tail) {
+        list.tail->next = nullptr;
+      }
+    } else {
+      // case 2/4: node is in the middle or at the head
+      // set the prev pointer of the node after the
+      // deletion target to the node before it
+      deletionTarget->next->prev = deletionTarget->prev;
+    }
+
+    delete deletionTarget;
+  }
 }
 
 // traverse the list in ascending order and print to outputFile
-void displayAscending (const NodeList &list, std::ofstream &outputFile) {
-    // start at the head
-    Node* curr = list.head;
+void displayAscending(const NodeList &list, std::ofstream &outputFile) {
+  // start at the head
+  Node *curr = list.head;
 
-    // for each node, print the name to the file and advance to the next node
-    while (curr) {
-        outputFile << curr->name << std::endl;
-        curr = curr->next;
-    }
+  // for each node, print the name to the file and advance to the next node
+  while (curr) {
+    outputFile << curr->name << std::endl;
+    curr = curr->next;
+  }
 }
 
 // traverse the list in descending order and print to outputFile
-void displayDescending (const NodeList &list, std::ofstream &outputFile) {
-    // start at the tail
-    Node* curr = list.tail;
+void displayDescending(const NodeList &list, std::ofstream &outputFile) {
+  // start at the tail
+  Node *curr = list.tail;
 
-    // for each node, print the name to the file and advance (retreat?) to the previous node
-    while (curr) {
-        outputFile << curr->name << std::endl;
-        curr = curr->prev;
-    }
+  // for each node, print the name to the file and advance (retreat?) to the
+  // previous node
+  while (curr) {
+    outputFile << curr->name << std::endl;
+    curr = curr->prev;
+  }
 }
 
-void convertToLower (std::string &str) {
-    std::transform(str.begin(), str.end(), str.begin(),
-        [](unsigned char c){ return std::tolower(c); }
-    );
+void convertToLower(std::string &str) {
+  std::transform(str.begin(), str.end(), str.begin(),
+                 [](unsigned char c) { return std::tolower(c); });
 }
 
-int main () {
-    // initialize empty list
-    NodeList list;
+int main() {
+  // initialize empty list
+  NodeList list;
 
-    // build list from input file
-    std::ifstream inputFile ("input.txt");
-    std::string lineText;
+  // build list from input file
+  std::ifstream inputFile("input.txt");
+  std::string lineText;
 
-    // for each line in the file:
-    // if it starts with "delete ", call deleteNode()
-    // else call insertNode()
-    while (std::getline (inputFile, lineText)) {
-        convertToLower(lineText);
+  // for each line in the file:
+  // if it starts with "delete ", call deleteNode()
+  // else call insertNode()
+  while (std::getline(inputFile, lineText)) {
+    convertToLower(lineText);
 
-        if (lineText.substr(0, 7) == "delete ") {
-            deleteNode(list, lineText.substr(7));
-        } else {
-            insertNode(list, lineText);
-        }
+    if (lineText.substr(0, 7) == "delete ") {
+      deleteNode(list, lineText.substr(7));
+    } else {
+      insertNode(list, lineText);
     }
-    inputFile.close();
+  }
+  inputFile.close();
 
-    // output: only runs on non-empty lists
-    if (list.head) {
-        // open target output file
-        std::ofstream outputFile ("output.txt");
+  // output: only runs on non-empty lists
+  if (list.head) {
+    // open target output file
+    std::ofstream outputFile("output.txt");
 
-        // display list in ascending order
-        displayAscending(list, outputFile);
+    // display list in ascending order
+    displayAscending(list, outputFile);
 
-        // display separator
-        outputFile << "=============" << std::endl;
+    // display separator
+    outputFile << "=============" << std::endl;
 
-        // display list in descending order
-        displayDescending(list, outputFile);
+    // display list in descending order
+    displayDescending(list, outputFile);
 
-        outputFile.close();
-    }
+    outputFile.close();
+  }
 
-    return 0;
+  return 0;
 }
--- quiz1.cpp
+++ quiz1.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 
-
 int main() {
-    float var = 40.5;
-    float *ptr1;
-    float **ptr2;
+  float var = 40.5;
+  float *ptr1;
+  float **ptr2;
 
-    ptr1 = &var;
-    ptr2 = &ptr1;
+  ptr1 = &var;
+  ptr2 = &ptr1;
 
-    std::cout << ***&ptr2;
+  std::cout << ***&ptr2;
 
-    return 0;
+  return 0;
 }
